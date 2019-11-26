4f32a56746ab369200674b5592c863836b2e4f0a
AdrianTofan <adrianvasile.tofan@gmail.com>
2019-11-26 02:39:45 +0000

    Fixed buttons on pop-up window. They work, but there is a problem with the panel.

--- Assets/Scripts/PanelOperator.cs
+++ Assets/Scripts/PanelOperator.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PanelOperator : MonoBehaviour
 {
     public GameObject Panel;
+    public GameObject button;
 
     public void OpenPanel()
     {
-        if(Panel != null)
+        if(Panel != null && button!=null)
         {
             Panel.SetActive(true);
+            button.SetActive(true);
         }
     }
 }
